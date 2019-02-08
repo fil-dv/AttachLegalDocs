6b726e29e7842f08beb78a42c1fb68a738307b8f
Dmitry Filonenko <feelll@bigmir.net>
2019-02-08 16:36:32 +0200

    Structure is ready

---
+++ AttachSolution/DbLayer/Models/Insert/InsertValues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbLayer.Models.Insert
+{
+    public class InsertValues
+    {
+        public string Dogovor_ID { get; set; } // номер договора
+        public string comment1 { get; set; } // Полное имя (для коммента)
+        public string comment2 { get; set; } // Полный путь
+        public string comment3 { get; set; } // Имя (сиквенс)
+        public double comment4 { get; set; }
+    }
+}
---
+++ AttachSolution/UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,92 @@
+﻿using QUp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UI.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region properties
+
+
+        bool _isReady = false;
+        public bool IsReady
+        {
+            get { return _isReady; }
+            set
+            {
+                _isReady = value;
+                OnPropertyChanged();
+            }
+        }
+
+        string _storageFolderName = "Archive/Legal.";
+        public string StorageFolderName
+        {
+            get { return _storageFolderName; }
+            set
+            {
+                _storageFolderName = value;
+                CheckIsReady();
+                OnPropertyChanged();
+                
+            }
+        }
+
+        string _path = String.Empty;
+        public string PathToLocalFolder
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                CheckIsReady();
+                OnPropertyChanged();                
+            }
+        }
+
+        #endregion
+
+        void CheckIsReady()
+        {
+            IsReady = (StorageFolderName.Where(x => Char.IsDigit(x)).Any() && PathToLocalFolder.Length > 0); 
+        }
+
+        ICommand _getPath;
+        public ICommand GetPath
+        {
+            get
+            {
+                if (_getPath == null)
+                {
+                    _getPath = new RelayCommand(
+                    p => true,
+                    p => GetPathToFile());
+                }
+                return _getPath;
+            }
+        }
+
+        void GetPathToFile()
+        {
+            PathToLocalFolder = "return";
+            MessageBox.Show(PathToLocalFolder);            
+        }
+
+    }
+}
